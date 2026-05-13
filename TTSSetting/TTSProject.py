c5c9cde9060037973aa8aa72baff128423c4b522
CelestialAspirePeak <celestialaspeak@outlook.com>
2026-05-13 22:21:50 +0800

    Update:CoTTS -> GSV-TTS-Lite
    TTS(语音播报)更新：把停止开发的CoTTS迁移至GSV

---
+++ TTSSetting/TTSProject.py
@@ -0,0 +1,9 @@
+from CAPQuakeCOTTS import CoTTS,Qwcont,Mao
+from CAPQuakeCOTTS.old import WS
+
+from CAPQuakeGPTSTTS import MediaCT
+from CAPQuakeGPTSTTS.ICL import API
+from CAPQuakeGPTSTTS.ICL import Talk,Model
+
+def LookTTS(Version):
+	return Version
