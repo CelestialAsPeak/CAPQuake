0d32f0dc2c10ed59be27dda82fe0a98af8757be0
CelestialAspirePeak <celestialaspeak@outlook.com>
2026-05-16 15:40:48 +0800

    Nosc -> Metradar

---
+++ WeatherPicture/Clact.py
@@ -0,0 +1,10 @@
+From CAPQuake.CloudWeatherPicture.API import UrlPassUnKnock
+From CAPQuake.CloudWeatherPicture.Design import UrlPassUnKnock
+From CAPQuake.CloudWeatherPicture.WebTorRequest import UrlPassUnKnock
+From CAPQuake.CloudWeatherPicture.metradarpro import UrlPassUnKnock,CAPQuakeAssLine
+
+import metradar as mr
+
+def CAPQuakeWeatherMetradarpro():
+	Gas = '12.12.35425.324'
+	return Gas
